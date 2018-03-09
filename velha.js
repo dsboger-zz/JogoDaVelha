5b45b8f84f4dbf9aacf2d2c1006f41d9689493c6
gustavomac89 <gustavomac89@gmail.com>
2018-03-09 20:28:35 -0300

    Coloquei o temporizador!

--- velha.js
+++ velha.js
@@ -1,14 +1,33 @@
 
 var vez = "x";
+var tempo = 20;
+var x;
+
+function relogio(){
+	tempo = tempo - 1;
+	document.getElementById('timer').innerText = tempo;
+	if (tempo == 0) {
+		document.getElementById('gameover').innerText = 'VOCÊ PERDEU!!!!!';
+		clearInterval(x);
+		tempo = 0
+		var all = document.getElementsByTagName('button');
+		for (var i=0, max=all.length; i < max; i++) {
+     	all[i].disabled = true;
+	}
+}
+}
 
 function clicado(botao) {
+
 	if (botao.innerText != "")
 		return; // botão já clicado
 		
 	if (vez == "x") {
+		tempo = 21;
 		botao.innerText = "x";
 		vez = "o";
 	} else {
+		tempo = 21;
 		botao.innerText = "o";
 		vez = "x";
 	}
@@ -17,4 +36,6 @@ function clicado(botao) {
 function inicia(){
 	document.getElementById('jogo').style.display = "grid";
 	document.getElementById('boas-vindas').style.display = "none";
+	x = setInterval(relogio, 1000);
 }
+
