54c9c1caef9e3dadf420ba495d16018ac37565a7
Unknown <gabriel.bunn@gmail.com>
2018-03-09 20:48:03 -0300

    Reverter
    Adicionado botão de reverter

--- velha.js
+++ velha.js
@@ -1,5 +1,6 @@
 
 var vez = "x";
+
 var tempo = 20;
 var x;
 
@@ -21,6 +22,9 @@ var pontoO = 0;
 var pontoX = 0;
 var empate = 0;
 
+var ultima;
+
+
 function clicado(botao) {
 
 	if (botao.innerText != "")
@@ -35,6 +39,7 @@ function clicado(botao) {
 		botao.innerText = "o";
 		vez = "x";
 	} 
+	ultima = botao;
 }
 
 function marcarX () {
@@ -71,5 +76,15 @@ function depoisqueacabareinicia() {
 	document.getElementById("bt_2_1").innerText = "";
 	document.getElementById("bt_2_2").innerText = "";
 	
-	
+}
+
+function desfazendo(refazer){
+    ultima.innerText = "";
+    
+    if(vez == "x"){
+        vez = "o"
+    } else {
+        vez = "x"
+    }
+
 }
